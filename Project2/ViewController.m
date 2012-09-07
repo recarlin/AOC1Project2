37a5368e2c63b410bf79aa06b3acc56cac0ebbaa
Russell Carlin <recarlin@fullsail.com>
2012-09-06 23:25:36 -0400

    Issues with my first project, but got it on the right settings now.

---
+++ Project2/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  Project2
+//
+//  Created by Russell Carlin on 9/6/12.
+//  Copyright (c) 2012 Russell Carlin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    self.view.backgroundColor = [UIColor brownColor];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
